36fd5052d6196103d095c76d484ebc65be12efd2
Jonathan Guyot Bourgeois <j.guyot-bourgeois@libr.im>
2022-01-23 00:38:06 +0100

    ajout d'une balise main et de plusieurs sections

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,5 @@
+const mainUrl = "http://10.0.0.147:8000/api/v1/titles/";
 
+const buttonMoreInfo = document.getElementById('btn-more-info');
+const buttonArrowLeft = document.getElementsByClassName('arrow-left');
+const buttonArrowRight = document.getElementsByClassName('arrow-right');
